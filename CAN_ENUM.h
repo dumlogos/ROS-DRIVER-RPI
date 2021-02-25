ba48532ac4e4994a1238267794c95842bf45d846
Andrew <andkudry@gmail.com>
2021-02-25 18:00:32 +0300

    CAN identificator defined

--- CAN_ENUM.h
+++ CAN_ENUM.h
@@ -2,34 +2,102 @@
 #define CAN_ENUM_H
 
 enum CAN_ID{
-    All,
-    T_Angle = 0x123,
-    T_Velocity,
-    T_StartBreak,
-    T_AngleProportionalRatio,
-    T_AngleDifferentialRatio,
-    T_AngleIntegralRatio,
-    T_VelocityProportionalRatio,
-    T_VelocityDifferentialRatio,
-    T_VelocityIntegralRatio,
-    T_ClearPosition = 0x12C,
-    T_CleanPlot = 0x12F,
-    R_Angle = 0x133,
-    R_Velocity,
-    R_StartBreak,
-    R_CleanPlot = 0x13F
+    /***********Mutual range************/
+
+        All,
+        Heartbeat                           = 0x010,
+
+    /*************RPi range*************/
+
+    //Common commands
+        Init                                = 0x100,
+        DeInit,
+        Error,
+        EmergencyStop,
+
+    //GUI commands
+        CleanPlot,
+
+    /*************Controller 1 range*************/
+        CAN_STM1 = 0x300,
+
+    /*************Controller 2 range*************/
+        CAN_STM2 = CAN_STM1 + 0x80,
+
+    /*************Controller 3 range*************/
+        CAN_STM3 = CAN_STM2 + 0x80,
+
+    /*************Controller 4 range*************/
+        CAN_STM4 = CAN_STM3 + 0x80,
+
+    /*************Controller 5 range*************/
+        CAN_STM5 = CAN_STM4 + 0x80,
+
+    /*************Controller 6 range*************/
+        CAN_STM6 = CAN_STM5 + 0x80
+
+};
+
+
+enum ControllerCommands{
+        ConnectionAcknowledge,
+        Disconnection,
+
+
+    //Robot control commands
+        MovingStart,
+        MovingStop,
+
+    //Controller commands
+        HeartbeatRespond,
+        RegulatorRatioTransmitRequest,
+        RegulatorRatioTransmitAcknowledge,
+        RegulatorRatioReceiveRequest,
+        RegulatorRatioReceiveAcknowledge,
+        MovingStartedAcknowledge,
+        MovingFinishedAcknowedge,
+        DriverStop
+};
+
+enum ControllerData{
+        T_Position = DriverStop + 1,
+        T_Speed,
+        R_Position,
+        R_Speed,
+
+        T_PositionProportionalRatio,
+        T_PositionDifferentialRatio,
+        T_PositionIntegralRatio,
+        T_SpeedProportionalRatio,
+        T_SpeedDifferentialRatio,
+        T_SpeedIntegralRatio,
+
+        R_PositionProportionalRatio,
+        R_PositionDifferentialRatio,
+        R_PositionIntegralRatio,
+        R_SpeedProportionalRatio,
+        R_SpeedDifferentialRatio,
+        R_SpeedIntegralRatio
 };
 
 enum DriverState{
-   STOP,
-   START
+       STOP,
+       START
 };
 
 enum CAN_IFace{
-  VCAN,
-  CAN0
+       VCAN,
+       CAN0
 };
 
+enum ControllerList{
+    STM1 = 1,
+    STM2,
+    STM3,
+    STM4,
+    STM5,
+    STM6
+};
 
 
 #endif // CAN_ENUM_H
